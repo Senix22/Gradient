b7605d6592abe97c58c93f02e760ec46b862f504
Senix22 <senix2276@gmail.com>
2021-01-25 21:26:34 +0200

    Initial commit

---
+++ app/src/main/java/com/senix22/fade/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.senix22.fade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
